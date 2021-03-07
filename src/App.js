de72e8edf8e9cae3a2af3fee7f48f7263768da3a
Ishraq Hussain <md.ishraq.hussain@accenture.com>
2021-03-07 17:49:44 +0530

    Commit test v3 changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gift a Gift gift - Just maybe - maybe
+          Gift a Gift gift - Just maybe - maybe try to check what's wrong
         </p>
         <a
           className="App-link"
